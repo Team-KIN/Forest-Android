8a8bfb9a05a025bcdf0ac6f34df85cab5644a294
yeongun130 <yeongunj625@gmail.com>
2023-12-07 00:16:55 +0900

    :heavy_plus_sign: color resource 추가

---
+++ presentation/src/main/java/com/kin/presentation/ui/theme/ColorTheme.kt
@@ -0,0 +1,16 @@
+package com.kin.presentation.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+abstract class ColorTheme {
+    abstract val PRIMARY: Color
+
+    abstract val SECONDARY: Color
+
+    abstract val ERROR: Color
+
+    abstract val FOCUS: Color
+
+    abstract val WHITE: Color
+    abstract val BLACK: Color
+}
---
+++ presentation/src/main/java/com/kin/presentation/ui/theme/color/LightColor.kt
@@ -0,0 +1,18 @@
+package com.kin.presentation.ui.theme.color
+
+import androidx.compose.ui.graphics.Color
+import com.kin.presentation.ui.theme.ColorTheme
+
+object LightColor: ColorTheme() {
+    override val PRIMARY = Color(0xFFA3D487)
+
+    override val SECONDARY = Color(0xFF96A989)
+
+    override val ERROR = Color(0xFFFF1A1A)
+
+    override val FOCUS = Color(0xFF007AF5)
+
+    override val WHITE = Color(0xFFFFFFFF)
+
+    override val BLACK = Color(0xFF000000)
+}
