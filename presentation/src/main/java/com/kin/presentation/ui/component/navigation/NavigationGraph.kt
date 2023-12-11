2d99516eef8753af806bf6bbc87e4efe3f880773
yeongun130 <yeongunj625@gmail.com>
2023-12-11 20:15:32 +0900

    :sparkles: bottom navigation

---
+++ presentation/src/main/java/com/kin/presentation/ui/component/navigation/NavigationGraph.kt
@@ -0,0 +1,20 @@
+package com.kin.presentation.ui.component.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.kin.presentation.ui.change_password.screen.ChangePasswordScreen
+import com.kin.presentation.ui.group_list.screen.GroupListScreen
+
+@Composable
+fun NavigationGraph(navController: NavHostController) {
+    NavHost(navController = navController, startDestination = BottomNavItem.GroupList.screenRoute) {
+        composable(BottomNavItem.GroupList.screenRoute) {
+            GroupListScreen()
+        }
+        composable(BottomNavItem.MyPage.screenRoute) {
+            ChangePasswordScreen()
+        }
+    }
+}
