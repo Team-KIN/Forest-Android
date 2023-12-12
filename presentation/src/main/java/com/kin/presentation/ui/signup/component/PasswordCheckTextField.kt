3c0a4bee0e0448b024b5a8df6d9e436a8e14508e
Kim Dae Jin <105362474+kimdaejin1@users.noreply.github.com>
2023-12-12 17:54:43 +0900

    :fire: 불필요한 임포트 삭제

--- presentation/src/main/java/com/kin/presentation/ui/signup/component/PasswordCheckTextField.kt
+++ presentation/src/main/java/com/kin/presentation/ui/signup/component/PasswordCheckTextField.kt
@@ -19,7 +19,6 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.kin.presentation.ui.auth.component.PasswordTextField
 import com.kin.presentation.ui.change_password.component.CheckNewPasswordTextField
 import com.kin.presentation.ui.theme.color.LightColor
 import com.teamgrapefruit.presentation.R
