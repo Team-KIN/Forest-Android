7ab7cb469970df086ce38dca8513b493e0f44126
Kim Dae Jin <105362474+kimdaejin1@users.noreply.github.com>
2023-12-12 17:03:17 +0900

    :fire: NicknameTextField 추가

---
+++ presentation/src/main/java/com/kin/presentation/ui/signup/component/NicknameTextField.kt
@@ -0,0 +1,52 @@
+package com.kin.presentation.ui.signup.component
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.kin.presentation.ui.theme.color.LightColor
+import com.teamgrapefruit.presentation.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun NicknameTextField() {
+    var text by remember { mutableStateOf("") }
+
+    Box(modifier = Modifier.width(320.dp)) {
+        Column {
+            Text(
+                text = stringResource(R.string.nickname_text),
+                color = LightColor.SECONDARY,
+                fontSize = 14.sp
+            )
+
+            TextField(
+                value = text,
+                onValueChange = {text = it},
+                modifier = Modifier.fillMaxWidth(),
+                colors = TextFieldDefaults.outlinedTextFieldColors()
+            )
+        }
+    }
+
+}
+
+@Preview
+@Composable
+fun NicknameTextFieldPreview(){
+    NicknameTextField()
+}
