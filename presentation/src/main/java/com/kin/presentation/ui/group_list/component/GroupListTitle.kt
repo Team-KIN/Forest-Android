e1622e9faf8ea66471d744895a703a4e6a9aa648
yeongun130 <yeongunj625@gmail.com>
2023-12-12 20:23:58 +0900

    :ambulance: text stringResource로 변경

--- presentation/src/main/java/com/kin/presentation/ui/group_list/component/GroupListTitle.kt
+++ presentation/src/main/java/com/kin/presentation/ui/group_list/component/GroupListTitle.kt
@@ -3,16 +3,18 @@ package com.kin.presentation.ui.group_list.component
 import androidx.compose.foundation.layout.Box
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.kin.presentation.ui.theme.color.LightColor
+import com.teamgrapefruit.presentation.R
 
 @Composable
 fun GroupListTitle() {
     Box() {
         Text(
-            text = "숲에 함께하기",
+            text = stringResource(R.string.join_group_text),
             fontSize = 18.sp,
             fontWeight = FontWeight.Bold,
             color = LightColor.BLACK
