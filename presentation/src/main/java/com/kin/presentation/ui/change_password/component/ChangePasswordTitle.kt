40e64fe5b4f8917ad4662267bd40b54b6a3da409
yeongun130 <yeongunj625@gmail.com>
2023-12-15 15:42:12 +0900

    :recycle: modify text color

--- presentation/src/main/java/com/kin/presentation/ui/change_password/component/ChangePasswordTitle.kt
+++ presentation/src/main/java/com/kin/presentation/ui/change_password/component/ChangePasswordTitle.kt
@@ -7,6 +7,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
+import com.kin.presentation.ui.theme.color.LightColor
 import com.teamgrapefruit.presentation.R
 
 @Composable
@@ -15,7 +16,8 @@ fun ChangePasswordTitle() {
         Text(
             text = stringResource(R.string.change_password_text),
             fontSize = 24.sp,
-            fontWeight = FontWeight.Bold
+            fontWeight = FontWeight.Bold,
+            color = LightColor.BLACK
         )
     }
 }
