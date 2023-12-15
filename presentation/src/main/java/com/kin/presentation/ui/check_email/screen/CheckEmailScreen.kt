f65757a3730930f7b1021e823c75a8b6a79d4c74
Kim Dae Jin <105362474+kimdaejin1@users.noreply.github.com>
2023-12-15 15:06:50 +0900

    :lipstick: 이메일 인증 Screen 추가

---
+++ presentation/src/main/java/com/kin/presentation/ui/check_email/screen/CheckEmailScreen.kt
@@ -0,0 +1,61 @@
+package com.kin.presentation.ui.check_email.screen
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.kin.presentation.ui.check_email.component.CheckEmailButton
+import com.kin.presentation.ui.check_email.component.CheckEmailTextField
+import com.kin.presentation.ui.check_email.component.CheckEmailTitle
+import com.kin.presentation.ui.component.back_page.BackPage
+import com.kin.presentation.ui.theme.color.LightColor
+
+@Composable
+fun CheckEmailScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(LightColor.WHITE)
+    ){
+        Column {
+            Row(
+                modifier = Modifier.padding(top = 18.dp)
+            ){
+                Spacer(modifier = Modifier.width(12.dp))
+                BackPage()
+            }
+            Column (
+                modifier = Modifier.padding(20.dp)
+            ){
+                CheckEmailTitle()
+            }
+            Column (
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ){
+                Spacer(modifier = Modifier.height(155.dp))
+                CheckEmailTextField()
+                Spacer(modifier = Modifier.height(80.dp))
+                CheckEmailButton {}
+            }
+        }
+    }
+
+}
+
+@Preview
+@Composable
+fun CheckEmailScreenPreview() {
+    CheckEmailScreen()
+}
