ce1a979bf9dd55b9e83833a868115e69c48e5ddc
yeongun130 <yeongunj625@gmail.com>
2023-12-19 17:29:41 +0900

    :lipstick: home import update

--- presentation/src/main/java/com/kin/presentation/ui/home/component/ExtraList.kt
+++ presentation/src/main/java/com/kin/presentation/ui/home/component/ExtraList.kt
@@ -1,16 +1,12 @@
 package com.kin.presentation.ui.home.component
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBarColors
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -18,8 +14,8 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.kin.presentation.R
 import com.kin.presentation.ui.theme.color.LightColor
-import com.teamgrapefruit.presentation.R
 
 @Composable
 fun ExtraList() {
