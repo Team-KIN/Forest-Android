8400e29ca4f262ddb1c623f50f77eaaf2437e378
yeongun130 <yeongunj625@gmail.com>
2023-12-19 16:55:06 +0900

    :sparkles: login logic - activity

--- presentation/src/main/java/com/kin/presentation/ui/login/LoginActivity.kt
+++ presentation/src/main/java/com/kin/presentation/ui/login/LoginActivity.kt
@@ -1,2 +1,54 @@
 package com.kin.presentation.ui.login
 
+import android.content.Intent
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.kin.presentation.ui.base.BaseActivity
+import com.kin.presentation.ui.login.screen.LoginScreen
+import com.kin.presentation.ui.signup.SignUpActivity
+import com.kin.presentation.viewmodel.LoginViewModel
+import com.kin.presentation.viewmodel.util.Event
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+
+@AndroidEntryPoint
+class LoginActivity: BaseActivity() {
+    private val loginViewModel by viewModels<LoginViewModel>()
+
+    private lateinit var token: String
+    override fun init() {
+        lifecycleScope.launch {
+            loginViewModel.saveTokenResponse.collect {
+                if (it is Event.Success) {
+                    val deviceTokenSF = getSharedPreferences("deviceToken", MODE_PRIVATE)
+                    deviceTokenSF.edit().putString("device", token).apply()
+                    finish()
+                }
+            }
+        }
+        setContent {
+            LoginScreen(
+                context = this,
+                lifecycleScope = lifecycleScope,
+                viewModel = viewModel(LocalContext.current as LoginActivity),
+                onSignUpClick = {
+                    pageSignUp()
+                    finish()
+                },
+                onMainClick = {}
+            )
+        }
+    }
+
+    private fun pageSignUp() {
+        startActivity(
+            Intent(
+                this,
+                SignUpActivity::class.java
+            )
+        )
+    }
+}
