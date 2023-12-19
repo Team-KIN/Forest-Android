12a5a9600ba038f98d40392feb0237ab0eeae5f4
Kim Dae Jin <105362474+kimdaejin1@users.noreply.github.com>
2023-12-19 20:13:26 +0900

    :lipstick: 멤버 리스트 추가

---
+++ presentation/src/main/java/com/kin/presentation/ui/group_setting/item/GroupMemberItem.kt
@@ -0,0 +1,48 @@
+package com.kin.presentation.ui.group_setting.item
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.kin.presentation.ui.theme.color.LightColor
+import com.teamgrapefruit.presentation.R
+
+@Composable
+fun GroupMemberItem() {
+    Row {
+        Image(
+            painter = painterResource(id = R.drawable.profile_image),
+            contentDescription = "profile image",
+            modifier = Modifier
+                .width(24.dp)
+                .height(24.dp)
+                .padding(1.dp)
+                .clip(shape = CircleShape)
+        )
+        Spacer(modifier = Modifier.width(4.dp))
+        Text(
+            text = "홍길동",
+            fontSize = 15.sp,
+            fontWeight = FontWeight(500),
+            color = LightColor.BLACK
+            )
+    }
+}
+
+@Preview
+@Composable
+fun GroupMemberItemPreview() {
+    GroupMemberItem()
+}
