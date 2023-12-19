d757be7172098b1bc4518d3df82d602ff5604cdb
yeongun130 <yeongunj625@gmail.com>
2023-12-19 17:29:02 +0900

    :lipstick: group management import update

--- presentation/src/main/java/com/kin/presentation/ui/group_management/component/dialog/DropoutDialog.kt
+++ presentation/src/main/java/com/kin/presentation/ui/group_management/component/dialog/DropoutDialog.kt
@@ -1,8 +1,6 @@
 package com.kin.presentation.ui.group_management.component.dialog
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.AlertDialog
 import androidx.compose.material.Text
@@ -11,14 +9,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.kin.presentation.R
 import com.kin.presentation.ui.theme.color.LightColor
-import com.teamgrapefruit.presentation.R
 
 @Composable
 fun DropoutDialog(
