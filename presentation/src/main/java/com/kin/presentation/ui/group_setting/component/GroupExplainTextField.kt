8f5b191033f692216aa1dc31fa39bab233ef8171
Kim Dae Jin <105362474+kimdaejin1@users.noreply.github.com>
2023-12-19 17:52:40 +0900

    :lipstick: 그룹 설명 텍스트 필드 컴포넌트 추가

---
+++ presentation/src/main/java/com/kin/presentation/ui/group_setting/component/GroupExplainTextField.kt
@@ -0,0 +1,38 @@
+package com.kin.presentation.ui.group_setting.component
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.kin.presentation.ui.theme.color.LightColor
+
+@Composable
+fun GroupExplainTextField(text: String){
+    var text = text
+
+    TextField(
+        value = text,
+        onValueChange = { text = it },
+        modifier = Modifier
+            .fillMaxWidth(),
+        readOnly = false,
+        textStyle = TextStyle(
+            fontSize = 15.sp,
+            fontWeight = FontWeight(500),
+            color = LightColor.BLACK
+        )
+    )
+}
+
+@Preview
+@Composable
+fun GroupExplainTextFieldPreview(){
+    GroupExplainTextField("이 나무는 모시깽이하는 나무인데\n" +
+            "과연 이놈이 침엽수인지 활엽수인지\n" +
+            "저로서는 도저히 알 수가 없는 그러한 그런\n" +
+            "모시깽이한 모시깽이라고 할 수 있습니다")
+}
