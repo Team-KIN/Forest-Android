fd1ab90721d52dc05bd060ee2f4103bf6020e1bd
Kim Dae Jin <105362474+kimdaejin1@users.noreply.github.com>
2023-12-19 14:54:10 +0900

    :lipstick: 그룹 설정 타이틀 컴포넌트 추가

---
+++ presentation/src/main/java/com/kin/presentation/ui/group_setting/component/GroupSettingTitle.kt
@@ -0,0 +1,26 @@
+package com.kin.presentation.ui.group_setting.component
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.kin.presentation.ui.theme.color.LightColor
+import com.teamgrapefruit.presentation.R
+
+@Composable
+fun GroupSettingTitle() {
+    Text(
+        text = stringResource(id = R.string.group_setting_text),
+        fontSize = 18.sp,
+        fontWeight = FontWeight(700),
+        color = LightColor.BLACK
+    )
+}
+
+@Preview
+@Composable
+fun GroupSettingTitlePreview() {
+    GroupSettingTitle()
+}
