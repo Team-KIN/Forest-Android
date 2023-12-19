ee2bc14562b4c864f4cf10c162086a461d9dc2ba
yeongun130 <yeongunj625@gmail.com>
2023-12-19 16:47:22 +0900

    :lipstick: login import 변경

--- presentation/src/main/java/com/kin/presentation/ui/login/component/LoginButton.kt
+++ presentation/src/main/java/com/kin/presentation/ui/login/component/LoginButton.kt
@@ -13,7 +13,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.kin.presentation.ui.theme.color.LightColor
-import com.teamgrapefruit.presentation.R
+import com.kin.presentation.R
 
 
 @Composable
