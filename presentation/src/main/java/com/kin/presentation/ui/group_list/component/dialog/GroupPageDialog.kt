f5717bfb2f8dc8a7c9c57f3ff7d7d8100e7f6909
yeongun130 <yeongunj625@gmail.com>
2023-12-19 16:50:50 +0900

    :lipstick: group list import 변경

--- presentation/src/main/java/com/kin/presentation/ui/group_list/component/dialog/GroupPageDialog.kt
+++ presentation/src/main/java/com/kin/presentation/ui/group_list/component/dialog/GroupPageDialog.kt
@@ -9,7 +9,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.teamgrapefruit.presentation.R
+import com.kin.presentation.R
 
 @Composable
 fun GroupPageDialog() {
