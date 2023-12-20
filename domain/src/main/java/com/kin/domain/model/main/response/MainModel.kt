9334d53da102a91f9bfaaa5bb75e00a657aab2f0
Kim Dae Jin <105362474+kimdaejin1@users.noreply.github.com>
2023-12-21 02:11:04 +0900

    :sparkles: 모델추가

---
+++ domain/src/main/java/com/kin/domain/model/main/response/MainModel.kt
@@ -0,0 +1,33 @@
+package com.kin.domain.model.main.response
+
+import com.google.gson.annotations.SerializedName
+import java.util.UUID
+
+data class MainModel(
+    @SerializedName("name")
+    val name: String,
+    @SerializedName("email")
+    val email: String,
+    @SerializedName("profile_url")
+    val profileUrl: String,
+    @SerializedName("groups")
+    val groups: Groups
+) {
+    data class Groups(
+        @SerializedName("id")
+        val id: UUID,
+        @SerializedName("name")
+        val name: String,
+        @SerializedName("headcount")
+        val headCount: Int,
+        @SerializedName("todo")
+        val todo: Int
+    )
+
+    fun Groups.toEntity() = MainModel.Groups(
+        id = id,
+        name = name,
+        headCount = headCount,
+        todo = todo
+    )
+}
